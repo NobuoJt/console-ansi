0ecc411cf7045606fb001a1381025ef3075a53fb
NobuoJt <mario.oga0512@gmail.com>
2024-09-12 21:31:59 +0900

    ADD feat ansi_alias index.ts+

---
+++ dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var console_ansi = /** @class */ (function () {
+    function console_ansi() {
+        this.RESET = "\u001B[0m";
+        this.BLACK = "\u001B[30m";
+        this.RED = "\u001B[31m";
+        this.GREEN = "\u001B[32m";
+        this.LIGHT_YELLOW = "\u001B[93m";
+        this.YELLOW = "\u001B[33m";
+        this.YELLOW_BACKGROUND = "\u001B[43m";
+        this.BLUE = "\u001B[34m";
+        this.PURPLE = "\u001B[35m";
+        this.CYAN = "\u001B[36m";
+        this.WHITE = "\u001B[37m";
+        this.BOLD = "\u001B[1m";
+        this.UNBOLD = "\u001B[21m";
+        this.UNDERLINE = "\u001B[4m";
+        this.STOP_UNDERLINE = "\u001B[24m";
+        this.BLINK = "\u001B[5m";
+    }
+    return console_ansi;
+}());
+exports.console_ansi = console_ansi;
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+"use strict";
+
+export class console_ansi{
+     RESET = "\u001B[0m";
+     BLACK = "\u001B[30m";
+     RED = "\u001B[31m";
+     GREEN = "\u001B[32m";
+     LIGHT_YELLOW = "\u001B[93m";
+     YELLOW = "\u001B[33m";
+     YELLOW_BACKGROUND = "\u001B[43m";
+     BLUE = "\u001B[34m";
+     PURPLE = "\u001B[35m";
+     CYAN = "\u001B[36m";
+     WHITE = "\u001B[37m";
+     BOLD = "\u001B[1m";
+     UNBOLD = "\u001B[21m";
+     UNDERLINE = "\u001B[4m";
+     STOP_UNDERLINE = "\u001B[24m";
+     BLINK = "\u001B[5m";
+}
